40e6ad17714e2dc66778da7d7f6ce0eae00b00b5
andrei <andrix2007@gmail.com>
2022-08-17 13:47:54 +0700

    Initial commit

---
+++ src/main/java/ru/itm/initbc/InitbcApplication.java
@@ -0,0 +1,13 @@
+package ru.itm.initbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/itm/initbc/InitbcApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itm.initbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
